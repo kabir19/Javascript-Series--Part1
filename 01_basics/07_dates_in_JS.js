b9d46de0fa25d211b44b900158fd482a4a461647
Kashyap Wadekar <31826553+kabir19@users.noreply.github.com>
2024-04-11 18:12:38 +0000

    date datatype covered in this section.

---
+++ 01_basics/07_dates_in_JS.js
@@ -0,0 +1,10 @@
+// Dates
+
+let myDate = new Date()
+
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
